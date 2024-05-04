59eaecd8e8276ac72ad0aa5b45ae48bb539e0052
Brooke Truong <bmtruong@ucsd.edu>
2024-05-03 18:06:32 -0700

    Completed expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,39 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const img = document.querySelector("#volume-controls img");
+  const vol = document.querySelector("#volume")
+  const jsConfetti = new JSConfetti();
+  //Change image when value of input changes
+  vol.addEventListener("input", function() {
+    let val = vol.value;
+    if (val == 0){
+      img.setAttribute('src', "assets/icons/volume-level-0.svg");
+    }
+    else if (val < 33){
+      img.setAttribute('src', "assets/icons/volume-level-1.svg");
+    }
+    else if (val < 67){
+      img.setAttribute('src', "assets/icons/volume-level-2.svg");
+    }
+    else{
+      img.setAttribute('src', "assets/icons/volume-level-3.svg");
+    }
+  });
+
+
+
+  //Play the audio when click button on party horn 
+  const play = document.querySelector("button");
+  const audio = document.querySelector("audio");
+  const option = document.getElementById("horn-select");
+  play.addEventListener('click', function(){
+    if (option.value == "party-horn"){
+      let val = vol.value;
+      let volumeLevel = val / 100;
+      audio.volume = volumeLevel;
+      audio.play();
+      jsConfetti.addConfetti();
+    }
+  })
 }
