f09c9b7bd5eb4928f0bf719f123da7228a056d29
Brooke Truong <bmtruong@ucsd.edu>
2024-05-04 17:47:54 -0700

    Fixed expose

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -6,6 +6,31 @@ function init() {
   const img = document.querySelector("#volume-controls img");
   const vol = document.querySelector("#volume")
   const jsConfetti = new JSConfetti();
+  const audio = document.querySelector("audio");
+  const selectionImage = document.getElementById("horn-select");
+  const image = document.querySelector("img");
+  //Change the image based on selection
+
+  selectionImage.addEventListener("change", function() {
+    switch (selectionImage.value){
+      case ("air-horn"):
+        image.setAttribute("src", "assets/images/air-horn.svg");
+        audio.setAttribute("src", "assets/audio/air-horn.mp3");
+        break;
+      case ("car-horn"):
+        image.setAttribute("src", "assets/images/car-horn.svg");
+        audio.setAttribute("src", "assets/audio/car-horn.mp3");
+        break;
+      case ("party-horn"):
+        image.setAttribute("src", "assets/images/party-horn.svg");
+        audio.setAttribute("src", "assets/audio/party-horn.mp3");
+        break;
+      default:
+        image.setAttribute("src", "assets/images/no-image.png");
+        break;
+    }
+  });
+
   //Change image when value of input changes
   vol.addEventListener("input", function() {
     let val = vol.value;
@@ -27,14 +52,13 @@ function init() {
 
   //Play the audio when click button on party horn 
   const play = document.querySelector("button");
-  const audio = document.querySelector("audio");
   const option = document.getElementById("horn-select");
   play.addEventListener('click', function(){
+    let val = vol.value;
+    let volumeLevel = val / 100;
+    audio.volume = volumeLevel;
+    audio.play();
     if (option.value == "party-horn"){
-      let val = vol.value;
-      let volumeLevel = val / 100;
-      audio.volume = volumeLevel;
-      audio.play();
       jsConfetti.addConfetti();
     }
   })
